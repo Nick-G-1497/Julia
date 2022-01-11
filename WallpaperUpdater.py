7d27ef26d216a126b2982bd25620cad94297779f
Nicholas Goralka <NickGoralka@gmail.com>
2022-01-10 22:17:55 -0700

    Windows compatibility

--- WallpaperUpdater.py
+++ WallpaperUpdater.py
@@ -6,10 +6,9 @@
 #  the environment of which is running and should still work.
 
 import os 
-import dbus
+# import dbus
 import argparse
-
-
+import ctypes
 
 ##
 # Class used to update your wallpaper
@@ -24,27 +23,31 @@ class WallpaperUpdater:
             # parser.add_argument('file', help='Wallpaper file name')
             # parser.add_argument('--plugin', '-p', help='Wallpaper plugin (default is org.kde.image)', default='org.kde.image')
             # args = parser.parse_args()
-            self._kde_set_wallpaper(filepath)
-      
-      ## 
-      # Set your wallpaper to whatever photo is given via the path
-      def _kde_set_wallpaper(self, filepath, plugin='org.kde.image'):
-            # file = "file:///" + absolute_path 
-            # os.system("gsettings set org.gnome.desktop.background picture-uri " + file)
+            self._windows_set_wallpaper(filepath)
+
+      def _windows_set_wallpaper(self, filepath):
             
-            jscript = """
-            var allDesktops = desktops();
-            print (allDesktops);
-            for (i=0;i<allDesktops.length;i++) {
-                  d = allDesktops[i];
-                  d.wallpaperPlugin = "%s";
-                  d.currentConfigGroup = Array("Wallpaper", "%s", "General");
-                  d.writeConfig("Image", "file://%s")
-            }
-            """
-            bus = dbus.SessionBus()
-            plasma = dbus.Interface(bus.get_object('org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
-            plasma.evaluateScript(jscript % (plugin, plugin, filepath))
+            ctypes.windll.user32.SystemParametersInfoW(20, 0, filepath , 0)
+      
+      # ## 
+      # # Set your wallpaper to whatever photo is given via the path
+      # def _kde_set_wallpaper(self, filepath, plugin='org.kde.image'):
+      #       # file = "file:///" + absolute_path 
+      #       # os.system("gsettings set org.gnome.desktop.background picture-uri " + file)
+            
+      #       jscript = """
+      #       var allDesktops = desktops();
+      #       print (allDesktops);
+      #       for (i=0;i<allDesktops.length;i++) {
+      #             d = allDesktops[i];
+      #             d.wallpaperPlugin = "%s";
+      #             d.currentConfigGroup = Array("Wallpaper", "%s", "General");
+      #             d.writeConfig("Image", "file://%s")
+      #       }
+      #       """
+      #       bus = dbus.SessionBus()
+      #       plasma = dbus.Interface(bus.get_object('org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
+      #       plasma.evaluateScript(jscript % (plugin, plugin, filepath))
             
       # def set_wallpaper(self, absolute_path):
       #   command = 'dconf write \"/org/gnome/desktop/background/picture-uri" \"\'' + absolute_path + "\'\""
